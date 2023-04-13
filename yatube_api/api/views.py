bb3a29670f4fa7857b872f4f6ad32eaf2206258f
JRushFobos <fobos_media@mail.ru>
2023-04-14 01:11:39 +0500

    project api_yatube ready for review #2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,9 @@
-from rest_framework import viewsets, permissions, status
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-
-
 from posts.models import Comment, Group, Post
-from .serializers import CommentSerializer, PostSerializer, GroupSerializer
+from rest_framework import permissions, viewsets
+
 from .permissions import IsOwnerOrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -19,18 +17,12 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     '''Вьюсет группы'''
 
     permission_classes = [permissions.IsAuthenticated]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-
-    def create(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def destroy(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -40,7 +32,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_queryset(self):
-        post = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         return Comment.objects.filter(post=post)
 
     def perform_create(self, serializer):
